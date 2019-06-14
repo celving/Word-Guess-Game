54bf2768d5d53d342e0eb71f307b70b8aea60894
Colin Elving <viewtifuljho@hotmail.com>
2019-06-14 12:53:58 -0500

    getElementByID not working hnnnggg

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -2,9 +2,12 @@
 var directionsText = document.getElementByID("directions-text")
 var wordArea = document.getElementByID("word-area")
 var guessesLeft = document.getElementByID("guesses-left")
-var wins = document.getElementByID("wins-text")
-var losses = document.getElementByID("losses-text")
+var winsText = document.getElementByID("wins-text")
+var lossesText = document.getElementByID("losses-text")
 
+//Create the directions text//
+
+var directionsText = "Press any key to get started!"
 //Create variables to hold the number of inital wins/losses//
 
 var wins = 0
@@ -12,17 +15,28 @@ var losses = 0
 
 //Create a thing that corresponds to the word area?//
 
-var wordArea
+var wordArea = ""
 
 //Add an event listener for the user's key presses//
 
-document.onkeyup = function(event) {
+document.onkeyup = function(event){
     var userGuess = event.key
+    var wordArea = wordsToGuess[1]
+    directionsText.textContent = ""
+    console.log(wordsToGuess)
+    console.log(userGuess)
+    winsText.textContent = "Wins: " + wins
+    lossesText.textContent = "Losses: " + losses
     //Create the logic of whether the user's key matches any of the letters in the word area? Probably using a lot of ifs//
 }
 
 
 //Logic to increment the wins/losses/number of guesses left?//
 
+//Display the number of wins/losses//
+
+
 
 //Something to contain a list of possible words for the user to guess//
+
+var wordsToGuess = ["apple", "banana", "orange"]
