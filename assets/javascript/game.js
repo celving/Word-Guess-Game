87536d3c38dc557daae31ade83645ff477d26d1c
Colin Elving <viewtifuljho@hotmail.com>
2019-06-15 17:25:31 -0500

    string split function

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,42 +1,56 @@
 // Create getElementByID variables for each element to link to the html //
 var directionsText = document.getElementById("directions-text")
 var wordArea = document.getElementById("word-area")
-var guessesLeft = document.getElementById("guesses-left")
+var guessesText = document.getElementById("guesses-left")
 var winsText = document.getElementById("wins-text")
 var lossesText = document.getElementById("losses-text")
 
 //Create the directions text//
 
-var directionsText = "Press any key to get started!"
+directionsText.textContent = "Press any key to get started!"
 //Create variables to hold the number of inital wins/losses//
 
 var wins = 0
 var losses = 0
+var guess = 0
 
 //Create a thing that corresponds to the word area?//
 
-var wordArea = ""
+
+
+//Something to contain a list of possible words for the user to guess//
+
+var wordsToGuess = ["apple ", "banana", "orange", "pear  ", "mango "]
 
 //Add an event listener for the user's key presses//
 
 document.onkeyup = function(event){
     var userGuess = event.key
-    var wordArea = wordsToGuess[1]
-    directionsText.textContent = ""
-    console.log(wordsToGuess)
-    console.log(userGuess)
+
+    directionsText.textContent = " "
+
+    var currentWord = wordsToGuess[Math.floor(Math.random()*wordsToGuess.length)];
+
+    console.log(currentWord)
+
+    splitWord(currentWord)
+
+
+    guessesText.textContent = "Guesses remaining: " + guess
     winsText.textContent = "Wins: " + wins
     lossesText.textContent = "Losses: " + losses
     //Create the logic of whether the user's key matches any of the letters in the word area? Probably using a lot of ifs//
-}
 
+}
+//Create a string split function to seperate the word into letters//
+function splitWord(word) {
+    var split = word.split("");
+    wordArea.textContent = "" + split[0] + "" + split[1] + "" + split[2] + "" + split[3] + "" + split[4] + "" + split[5]
+   
+}
 
 //Logic to increment the wins/losses/number of guesses left?//
 
 //Display the number of wins/losses//
 
 
-
-//Something to contain a list of possible words for the user to guess//
-
-var wordsToGuess = ["apple", "banana", "orange"]
