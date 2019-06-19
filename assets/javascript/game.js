5fe3a4250280e5b21d753f623d42a5d869b6b1e0
Colin Elving <viewtifuljho@hotmail.com>
2019-06-19 17:24:17 -0500

    game.js doneish

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,77 +1,175 @@
-// Create getElementByID variables for each element to link to the html //
+// Global variables //
 var directionsText = document.getElementById("directions-text")
 var wordArea = document.getElementById("word-area")
 var guessesText = document.getElementById("guesses-left")
 var winsText = document.getElementById("wins-text")
 var lossesText = document.getElementById("losses-text")
+var wrongGuesses = document.getElementById("wrong-guesses")
+var winCountdown = 5
 
-//Create the directions text//
+//a list of possible words for the user to guess//
+var wordsToGuess = ["shark", "cuttlefish", "plankton", "octopus", "stingray", "dolphin", ""]
 
-directionsText.textContent = "Press any key to get started!"
-//Create variables to hold the number of inital wins/losses//
+
+//Holds the current word to be guessed//
+
+var currentWord
+
+var displayedLetters = []
+
+var currentLetters = []
+
+//Holds the guessed letters for the current word//
+
+var guessedLetters = []
+
+
+//Create variables to hold the number of wins/losses//
 
 var wins = 0
 var losses = 0
 
-var guess = 12
 
-var lettersGuessed = [];
+//Create the directions text//
 
-//Something to contain a list of possible words for the user to guess//
+directionsText.textContent = "Press any key to get started!"
 
-var wordsToGuess = ["apple", "banana", "orange", "grapes", "mango"]
 
-//Add an event listener for the user's key presses//
+var guessesRemaining = 1
 
-document.onkeyup = function(event){
-    var userGuess = event.key
 
-    directionsText.textContent = " "
+
+//Functions//
+
+var startGame = function(){
+    wordArea.textContent = " ";
+    directionsText.textContent = " ";
+
+    currentWord = wordsToGuess[Math.floor(Math.random()*wordsToGuess.length)];
     
+    splitWord(currentWord);
+    
+    blankFill();
 
 
-    var currentWord = wordsToGuess[Math.floor(Math.random()*wordsToGuess.length)];
-
-    wordArea.textContent = " "
-
-
-
-    console.log(currentWord)
-
-    splitWord(currentWord)
-
-    guessesText.textContent = "Guesses remaining: " + guess
+    wordArea.textContent = displayedLetters.join(" ");
+    
+    
+    guessesText.textContent = "Guesses remaining: " + guessesRemaining
     winsText.textContent = "Wins: " + wins
     lossesText.textContent = "Losses: " + losses
-    
-    
-    
-    
-    if (guess < 1){
-        losses ++
-        guess = 12
-    }
-    //Create the logic of whether the user's key matches any of the letters in the word area? Probably using a lot of ifs//
 
-    
-    
-    guess --
+    winCountdown = currentLetters.length;
+
+    window.removeEventListener("keyup", startGame);
+
+    window.addEventListener("keyup", checkLetters);
 }
+
 //Create a string split function to seperate the word into letters//
 function splitWord(word) {
     var split = word.split("");
-    for (w = 0; w < split.length ; w++){
-        var wordArea = document.getElementById("word-area")
-        let underscoreBlank = document.createElement("div");
-        underscoreBlank.textContent = "_";
+    
+    guessesRemaining = split.length * 2;
 
-        wordArea.appendChild(underscoreBlank)
+    for (w = 0; w < split.length ; w++){
+        currentLetters.push(split[w])
+    }
+
+
+
+    
+    console.log(currentLetters)
+}
+
+//Create a function to fill the displayed letters with blanks
+
+var blankFill = function(){
+    for (b = 0; b < currentLetters.length; b++){
+        displayedLetters.push("_")
+    }
+}
+
+
+//Add event listeners for the user's key presses//
+
+window.addEventListener("keyup", startGame)
+
+//Function to check the user's keystroke against currentLetters[]//
+
+var checkLetters = function(){
+
+    
+    guessesText.textContent = "Guesses remaining: " + guessesRemaining
+    winsText.textContent = "Wins: " + wins
+    lossesText.textContent = "Losses: " + losses
+
+    
+    guessesRemaining --
+
+    var userGuess = event.key.toLowerCase();
+    for (var g = 0; g < currentLetters.length; g++) {
+        if (currentLetters[g] === userGuess ){
+            displayedLetters[g] = userGuess;
+
+            wordArea.textContent = displayedLetters.join(" ");
+
+            winCountdown--;
+            
+
+            console.log(winCountdown)
+        }
+
+
+
+    }
+    
+        guessedLetters.push(userGuess.toUpperCase())
+        
+        wrongGuesses.textContent = guessedLetters.join(" ")
+
+
+
+    if (winCountdown < 1){
+        wins ++
+        
+        window.addEventListener("keyup", startGame)
+
+        
+        window.removeEventListener("keyup", checkLetters);
+
+        directionsText.textContent = "Well done! Press any key to play again."
+
+        
+        wordArea.textContent = " ";
+        
+        currentLetters.length = 0;
+        directionsText.length = 0;
+        displayedLetters.length = 0;
+        wrongGuesses.textContent = " ";
+        guessedLetters.length = 0;
+
+    }
+
+    if (guessesRemaining <= 1){
+        losses ++
+
+        window.addEventListener("keyup", startGame)
+
+        
+        window.removeEventListener("keyup", checkLetters);
+
+        directionsText.textContent = "Too bad! Press any key to play again."
+
+        wordArea.textContent = " ";
+
+        currentLetters.length = 0;
+        directionsText.length = 0;
+        displayedLetters.length = 0;
+        wrongGuesses.textContent = " ";
+        guessedLetters.length = 0;
     }
 
 }
-
-//Logic to increment the wins/losses/number of guesses left?//
-
-//Display the number of wins/losses//
 
 
